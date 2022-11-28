dbb30af0cae497d06b20707c5677552766290883
Prasad Bhat <prasad.bhat@flipkart.com>
2022-11-28 15:09:35 +0530

    Refactor

--- vajram/src/main/java/com/flipkart/krystal/vajram/exec/VajramDefinition.java
+++ vajram/src/main/java/com/flipkart/krystal/vajram/exec/VajramDefinition.java
@@ -6,15 +6,21 @@ import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 import lombok.Getter;
 
+import java.util.Collection;
+
 final class VajramDefinition {
 
   @Getter private final Vajram<?> vajram;
 
   // TODO populate input resolvers from vajram
-  @Getter private final ImmutableCollection<InputResolver> inputResolvers;
+  @Getter private final Collection<InputResolver> inputResolvers;
 
   VajramDefinition(Vajram<?> vajram) {
     this.vajram = vajram;
-    this.inputResolvers = ImmutableList.of();
+    this.inputResolvers = resolveInputs();
+  }
+
+  private Collection<InputResolver> resolveInputs() {
+    return vajram.getSimpleInputResolvers();
   }
 }
