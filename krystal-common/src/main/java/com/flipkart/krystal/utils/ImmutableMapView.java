c5b6173ff327bd21181d4720ce14ed0d99492145
ramanvesh <ram.anvesh@flipkart.com>
2023-01-20 12:06:34 +0530

    Add support for deep vajram dependency concurrency

--- krystal-common/src/main/java/com/flipkart/krystal/utils/ImmutableMapView.java
+++ krystal-common/src/main/java/com/flipkart/krystal/utils/ImmutableMapView.java
@@ -1,50 +1,22 @@
 package com.flipkart.krystal.utils;
 
-import static com.google.common.collect.ImmutableList.toImmutableList;
-import static com.google.common.collect.ImmutableSet.toImmutableSet;
-
-import com.google.common.collect.ImmutableCollection;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import java.util.AbstractMap;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Predicate;
-import org.checkerframework.checker.nullness.qual.Nullable;
 
-public final class ImmutableMapView<K, V> extends AbstractMap<K, V> {
+public final class ImmutableMapView<K, V> implements Map<K, V> {
 
-  private static final Predicate<Object> DEFAULT_KEY_FILTER = null;
-  private static final ImmutableMapView<?, ?> EMPTY = copyOf(ImmutableMap.of());
+  private static final ImmutableMapView<?, ?> EMPTY = viewOf(ImmutableMap.of());
 
   private final Map<K, V> delegate;
-  private final Predicate<Object> keyFilter;
-  private Integer size;
-  private Set<K> keySet;
-  private Set<Entry<K, V>> entrySet;
-  private ImmutableList<V> values;
 
-  private ImmutableMapView(Map<K, V> delegate, @Nullable Predicate<Object> keyFilter) {
+  private ImmutableMapView(Map<K, V> delegate) {
     this.delegate = delegate;
-    this.keyFilter = keyFilter;
   }
 
-  public static <K, V> ImmutableMapView<K, V> copyOf(Map<K, V> map) {
-    return filteredCopyOf(map, DEFAULT_KEY_FILTER);
-  }
-
-  public static <K, V> ImmutableMapView<K, V> filteredCopyOf(
-      Map<K, V> map, Predicate<Object> keyFilter) {
-    return new ImmutableMapView<>(ImmutableMap.copyOf(map), keyFilter);
-  }
-
-  public static <K, V> ImmutableMapView<K, V> copyOf(ImmutableMapView<K, V> map) {
-    return filteredCopyOf(map, DEFAULT_KEY_FILTER);
-  }
-
-  public static <K, V> ImmutableMapView<K, V> filteredCopyOf(
-      ImmutableMapView<K, V> map, Predicate<Object> keyFilter) {
-    return new ImmutableMapView<>(map, keyFilter);
+  public static <K, V> ImmutableMapView<K, V> viewOf(Map<K, V> map) {
+    return new ImmutableMapView<>(map);
   }
 
   public static <K, V> ImmutableMapView<K, V> of() {
@@ -54,84 +26,68 @@ public final class ImmutableMapView<K, V> extends AbstractMap<K, V> {
 
   @Override
   public int size() {
-    if (size == null) {
-      if (keyFilter != null) {
-        this.size = Math.toIntExact(delegate.keySet().stream().filter(keyFilter).count());
-      } else {
-        this.size = delegate.size();
-      }
-    }
-    return size;
+    return delegate.size();
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return delegate.isEmpty();
   }
 
   @Override
   public boolean containsKey(Object key) {
-    return delegate.containsKey(key) && (keyFilter == null || keyFilter.test(key));
+    return delegate.containsKey(key);
+  }
+
+  @Override
+  public boolean containsValue(Object value) {
+    return delegate.containsValue(value);
   }
 
   @Override
   public V get(Object key) {
-    if (keyFilter != null && !keyFilter.test(key)) {
-      return null;
-    }
     return delegate.get(key);
+  }
+
+  @Override
+  public Set<K> keySet() {
+    return delegate.keySet();
+  }
+
+  @Override
+  public Set<Entry<K, V>> entrySet() {
+    return delegate.entrySet();
+  }
+
+  @Override
+  public Collection<V> values() {
+    return delegate.values();
+  }
+
+  @Override
+  public V put(K key, V value) {
+    throw uoe();
   }
 
   @Deprecated
   @Override
   public V remove(Object key) {
-    return uoe();
+    throw uoe();
   }
 
   @Override
   @Deprecated
   public void putAll(Map<? extends K, ? extends V> m) {
-    uoe();
+    throw uoe();
   }
 
   @Override
   @Deprecated
   public void clear() {
-    uoe();
+    throw uoe();
   }
 
-  @Override
-  public Set<K> keySet() {
-    if (keySet == null) {
-      if (keyFilter != null) {
-        this.keySet = delegate.keySet().stream().filter(keyFilter).collect(toImmutableSet());
-      } else {
-        this.keySet = delegate.keySet();
-      }
-    }
-    return keySet;
-  }
-
-  @Override
-  public Set<Entry<K, V>> entrySet() {
-    if (entrySet == null) {
-      if (keyFilter != null) {
-        this.entrySet =
-            delegate.entrySet().stream()
-                .filter(e -> keyFilter.test(e.getKey()))
-                .collect(toImmutableSet());
-      } else {
-        this.entrySet = delegate.entrySet();
-      }
-    }
-    return entrySet;
-  }
-
-  @Override
-  public ImmutableCollection<V> values() {
-    if (values == null) {
-      //noinspection SimplifyStreamApiCallChains
-      this.values = entrySet().stream().map(Entry::getValue).collect(toImmutableList());
-    }
-    return values;
-  }
-
-  private static <T> T uoe() {
+  private static UnsupportedOperationException uoe() {
     throw new UnsupportedOperationException();
   }
 }
--- krystex/src/main/java/com/flipkart/krystal/krystex/node/NodeDefinition.java
+++ krystex/src/main/java/com/flipkart/krystal/krystex/node/NodeDefinition.java
@@ -1,5 +1,8 @@
 package com.flipkart.krystal.krystex.node;
 
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+import com.flipkart.krystal.krystex.CallGraph;
 import com.flipkart.krystal.krystex.ResolverDefinition;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
@@ -9,4 +12,18 @@ public record NodeDefinition(
     NodeLogicId mainLogicNode,
     ImmutableMap<String, NodeId> dependencyNodes,
     ImmutableList<ResolverDefinition> resolverDefinitions,
-    NodeDefinitionRegistry nodeDefinitionRegistry) {}
+    NodeDefinitionRegistry nodeDefinitionRegistry) {
+  public CallGraph getCallGraph(CallGraph previousCalls) {
+    return new CallGraph(
+        nodeId,
+        resolverDefinitions.stream()
+            .map(
+                resolverDefinition -> {
+                  String dependencyName = resolverDefinition.dependencyName();
+                  NodeId depNodeId = dependencyNodes.get(dependencyName);
+                  NodeDefinition depNode = nodeDefinitionRegistry.get(depNodeId);
+                  return depNode.getCallGraph(previousCalls);
+                })
+            .collect(toImmutableList()));
+  }
+}
