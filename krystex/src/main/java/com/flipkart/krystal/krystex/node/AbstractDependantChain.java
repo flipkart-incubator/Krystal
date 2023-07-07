cc7f321ae6c0f3458cff2ae51285a26a8a3db12b
ramanvesh <ram.anvesh@flipkart.com>
2023-07-07 17:05:03 +0530

    Resolve Concurrent Modification Error

--- krystex/src/main/java/com/flipkart/krystal/krystex/node/AbstractDependantChain.java
+++ krystex/src/main/java/com/flipkart/krystal/krystex/node/AbstractDependantChain.java
@@ -1,18 +1,18 @@
 package com.flipkart.krystal.krystex.node;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public abstract sealed class AbstractDependantChain implements DependantChain
     permits DefaultDependantChain, DependantChainStart {
 
   private final Map<NodeId, Map<String, DependantChain>> dependenciesInternPool =
-      new LinkedHashMap<>();
+      new ConcurrentHashMap<>();
 
   @Override
   public DependantChain extend(NodeId nodeId, String dependencyName) {
     return dependenciesInternPool
-        .computeIfAbsent(nodeId, _n -> new LinkedHashMap<>())
+        .computeIfAbsent(nodeId, _n -> new ConcurrentHashMap<>())
         .computeIfAbsent(
             dependencyName, depName -> new DefaultDependantChain(nodeId, depName, this));
   }
