6443075f546717a414afb9fca8973c86e4b761d4
ramanvesh <ramanvesh@gmail.com>
2023-07-23 12:50:30 +0530

    Move common dependencies to parent build.gradle

--- krystex/src/test/java/com/flipkart/krystal/krystex/kryon/KryonExecutorTest.java
+++ krystex/src/test/java/com/flipkart/krystal/krystex/kryon/KryonExecutorTest.java
@@ -29,7 +29,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class KrystalKryonExecutorTest {
+class KryonExecutorTest {
 
   private static final Duration TIMEOUT = Duration.ofSeconds(1000);
 
