4f5df91c951f855d9a69df96d196823d9eb124b5
ramanvesh <ramanvesh@gmail.com>
2024-02-05 10:35:32 +0530

    Refactor SimpleInputResolverSpec to record

--- vajram/src/main/java/com/flipkart/krystal/vajram/facets/resolution/SimpleInputResolverSpec.java
+++ vajram/src/main/java/com/flipkart/krystal/vajram/facets/resolution/SimpleInputResolverSpec.java
@@ -6,8 +6,6 @@ import com.flipkart.krystal.vajram.facets.VajramFacetSpec;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
-import lombok.Getter;
-import lombok.experimental.Accessors;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
@@ -17,30 +15,10 @@ import org.checkerframework.checker.nullness.qual.Nullable;
  * @param <CV> The type of the vajram doing the resolution.
  * @param <DV> The type of the vajram whose input is being resolved.
  */
-@Getter
-@Accessors(fluent = true)
-public final class SimpleInputResolverSpec<
-    T, CV extends VajramRequest<?>, DV extends VajramRequest<?>> {
-
-  private final VajramFacetSpec<T, DV> targetInput;
-  private final List<VajramFacetSpec<?, CV>> sourceInputs;
-  private final List<SkipPredicate<?>> skipConditions;
-  private final @Nullable Function<List<ValueOrError<?>>, @Nullable T> transformer;
-
-  private final @Nullable Function<List<ValueOrError<?>>, ? extends Collection<? extends T>>
-      fanoutTransformer;
-
-  public SimpleInputResolverSpec(
-      VajramFacetSpec<T, DV> targetInput,
-      List<VajramFacetSpec<?, CV>> sourceInput,
-      List<SkipPredicate<?>> skipConditions,
-      @Nullable Function<List<ValueOrError<?>>, @Nullable T> transformer,
-      @Nullable Function<List<ValueOrError<?>>, ? extends Collection<? extends T>>
-          fanoutTransformer) {
-    this.targetInput = targetInput;
-    this.sourceInputs = sourceInput;
-    this.skipConditions = skipConditions;
-    this.transformer = transformer;
-    this.fanoutTransformer = fanoutTransformer;
-  }
-}
+public record SimpleInputResolverSpec<T, CV extends VajramRequest<?>, DV extends VajramRequest<?>>(
+    VajramFacetSpec<T, DV> targetInput,
+    List<VajramFacetSpec<?, CV>> sourceInputs,
+    List<SkipPredicate<?>> skipConditions,
+    @Nullable Function<List<ValueOrError<?>>, @Nullable T> transformer,
+    @Nullable Function<List<ValueOrError<?>>, ? extends Collection<? extends T>>
+        fanoutTransformer) {}
