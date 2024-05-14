b4db1d60a3e256a054784f3d71282102d8cfd64b
ramanvesh <ramanvesh@gmail.com>
2024-05-14 17:25:13 +0530

    Some code clean up

--- krystex/src/main/java/com/flipkart/krystal/krystex/kryon/KrystalExecutorCompletionListener.java
+++ krystex/src/main/java/com/flipkart/krystal/krystex/kryon/KrystalExecutorCompletionListener.java
@@ -3,7 +3,5 @@ package com.flipkart.krystal.krystex.kryon;
 /** Default implementation of Krystal executor which */
 public interface KrystalExecutorCompletionListener {
 
-  public default void onComplete() {
-    // do nothing
-  }
+  default void onComplete() {}
 }
