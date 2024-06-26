29c067a4d278376886d21cff4b2b543d88bc4dcc
ramanvesh <ramanvesh@gmail.com>
2024-06-27 03:36:51 +0530

    Delete ApplicationRequestContext

--- vajram/src/main/java/com/flipkart/krystal/vajram/ApplicationRequestContext.java
+++
@@ -1,9 +0,0 @@
-package com.flipkart.krystal.vajram;
-
-public interface ApplicationRequestContext {
-
-  /**
-   * @return The requestId of the request that this request context represents.
-   */
-  String requestId();
-}
