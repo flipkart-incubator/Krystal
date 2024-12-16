34ff99d6846c7aae1313475d36b14a1a8e6b05e4
ramanvesh <ramanvesh@gmail.com>
2024-12-16 17:41:10 +0530

    Fix nullchecker errors

--- krystal-common/src/main/java/com/flipkart/krystal/data/DepResponsesImpl.java
+++ krystal-common/src/main/java/com/flipkart/krystal/data/DepResponsesImpl.java
@@ -1,17 +1,20 @@
 package com.flipkart.krystal.data;
 
+import static com.flipkart.krystal.data.Errable.nil;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
 
 import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 public final class DepResponsesImpl<R extends Request<T>, T> implements DependencyResponses<R, T> {
 
   private final ImmutableList<RequestResponse<R, T>> requestResponses;
 
-  private ImmutableMap<ImmutableRequest<T>, Errable<T>> responsesAsMap;
+  private @MonotonicNonNull ImmutableMap<ImmutableRequest<T>, Errable<T>> responsesAsMap;
 
   public DepResponsesImpl(ImmutableList<RequestResponse<R, T>> requestResponses) {
     this.requestResponses = requestResponses;
@@ -29,7 +32,8 @@ public final class DepResponsesImpl<R extends Request<T>, T> implements Dependen
     if (responsesAsMap == null) {
       responsesAsMap = asMap();
     }
-    return responsesAsMap.get(request._build());
+    Errable<T> response = responsesAsMap.getOrDefault(request._build(), nil());
+    return response;
   }
 
   private ImmutableMap<ImmutableRequest<T>, Errable<T>> asMap() {
