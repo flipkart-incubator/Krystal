f0babb277847459f033d674269c9e25cccdd0f82
lightflash03 <vamgamer03@gmail.com>
2025-11-11 15:56:52 +0530

    Cleanup imports

--- vajram/vajram-samples/src/main/java/com/flipkart/krystal/vajram/samples/user/response_pojos/UserProfile.java
+++ vajram/vajram-samples/src/main/java/com/flipkart/krystal/vajram/samples/user/response_pojos/UserProfile.java
@@ -1,7 +1,3 @@
 package com.flipkart.krystal.vajram.samples.user.response_pojos;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.Value;
-
 public record UserProfile(String profileData) {}
