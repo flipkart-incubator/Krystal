6241dc95b7948a1bf136fe1d46bbb959301bc465
lightflash03 <vamgamer03@gmail.com>
2025-11-21 11:48:18 +0530

    Update GraphqlEntity marker class

--- vajram/extensions/graphql/vajram-graphql/src/main/java/com/flipkart/krystal/vajram/graphql/api/model/GraphQlEntity.java
+++ vajram/extensions/graphql/vajram-graphql/src/main/java/com/flipkart/krystal/vajram/graphql/api/model/GraphQlEntity.java
@@ -5,6 +5,4 @@ import com.flipkart.krystal.model.ModelClusterRoot;
 @ModelClusterRoot(
     immutableRoot = GraphQlEntity_Immut.class,
     builderRoot = GraphQlEntity_Immut.Builder.class)
-public interface GraphQlEntity<I extends GraphQlEntityId> extends GraphQlObject {
-  // No fixed id() method - entities define their own ID field method with custom names
-}
+public interface GraphQlEntity<I extends GraphQlEntityId> extends GraphQlObject {}
