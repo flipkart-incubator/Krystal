829c4a706d26a914fb8bd9bca0f89fa3c0dfe07b
lightflash03 <vamgamer03@gmail.com>
2025-12-09 16:33:18 +0530

    Add in the E2E test

--- vajram/extensions/graphql/vajram-graphql-samples/src/main/java/com/flipkart/krystal/vajram/graphql/samples/order/GetOrderSummary.java
+++ vajram/extensions/graphql/vajram-graphql-samples/src/main/java/com/flipkart/krystal/vajram/graphql/samples/order/GetOrderSummary.java
@@ -8,9 +8,19 @@ import com.flipkart.krystal.vajram.Vajram;
 import com.flipkart.krystal.vajram.facets.Output;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 
 @Vajram
 public abstract class GetOrderSummary extends ComputeVajramDef<GetOrderSummary_GQlFields> {
+
+  /** Unix epoch - January 1, 1970 00:00:00 UTC */
+  public static final OffsetDateTime UNIX_EPOCH_DATE_TIME =
+      OffsetDateTime.of(1970, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC);
+
+  /** Unix epoch date - January 1, 1970 */
+  public static final LocalDate UNIX_EPOCH_DATE = LocalDate.of(1970, 1, 1);
+
+  @SuppressWarnings("initialization.field.uninitialized")
   static class _Inputs {
     @IfAbsent(FAIL)
     OrderId id;
@@ -18,12 +28,10 @@ public abstract class GetOrderSummary extends ComputeVajramDef<GetOrderSummary_G
 
   @Output
   static GetOrderSummary_GQlFields output() {
-    OffsetDateTime currentDateTime = OffsetDateTime.now();
-    LocalDate currentDate = LocalDate.now();
     return GetOrderSummary_GQlFields.builder()
         .orderItemsCount(Long.MAX_VALUE)
-        .orderPlacedAt(currentDateTime)
-        .orderAcceptDate(currentDate)
+        .orderPlacedAt(UNIX_EPOCH_DATE_TIME)
+        .orderAcceptDate(UNIX_EPOCH_DATE)
         .build();
   }
 }
