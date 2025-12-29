e493b30f9f364976f4395c5d8f27e1b86cad165f
Ram Anvesh <2082745+RamAnvesh@users.noreply.github.com>
2025-12-29 11:09:27 +0530

    Remove static final constants of type KrystalException since now they can have stacktraces

--- krystal-common/src/main/java/com/flipkart/krystal/except/SkippedExecutionException.java
+++ krystal-common/src/main/java/com/flipkart/krystal/except/SkippedExecutionException.java
@@ -5,7 +5,4 @@ public class SkippedExecutionException extends KrystalException {
   public SkippedExecutionException(String message) {
     super(message);
   }
-
-  public static final SkippedExecutionException SKIPPED_EXECUTION_EXCEPTION =
-      new SkippedExecutionException("Execution was skipped due to a skip command.");
 }
