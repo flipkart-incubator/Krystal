4c6aad56b0ccf0aca187551cabe99d27e42b3086
Ram Anvesh <2082745+RamAnvesh@users.noreply.github.com>
2026-04-17 20:48:18 +0530

    Fix maven central publish issues

--- publishToMavenLocal.macOS.sh
+++ publishToMavenLocal.macOS.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-sed -i '' 's/com.flipkart.krystal.currentVersion/com.flipkart.krystal.previousVersion/g' build.gradle
-./gradlew publishToMavenLocal -PunsafeCompile=true --no-build-cache #Unsafe compile temporarily needed due to a checker-framework issue
-sed -i '' 's/com.flipkart.krystal.previousVersion/com.flipkart.krystal.currentVersion/g' build.gradle
-./gradlew publishToMavenLocal -PunsafeCompile=true --no-build-cache #Unsafe compile temporarily needed due to a checker-framework issue
+sed -i '' 's/com.flipkart.krystal.currentVersion/com.flipkart.krystal.previousVersion/g' build.gradle \
+&&
+#Unsafe compile temporarily needed due to a checker-framework issue
+./gradlew :vajram:vajram-codegen:publishToMavenLocal -PunsafeCompile=true --no-build-cache \
+&&
+sed -i '' 's/com.flipkart.krystal.previousVersion/com.flipkart.krystal.currentVersion/g' build.gradle \
+&&
+#Unsafe compile temporarily needed due to a checker-framework issue
+./gradlew publishToMavenLocal -PunsafeCompile=true --no-build-cache
