d035583b138c4693bcc3c834afc7777094902000
Ram Anvesh <2082745+RamAnvesh@users.noreply.github.com>
2026-04-18 13:01:07 +0530

    Simplify publishing mechanism

--- upgradeVersionLocal.macOS.sh
+++ upgradeVersionLocal.macOS.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-sed -i '' 's/com.flipkart.krystal.currentVersion/com.flipkart.krystal.previousVersion/g' build.gradle \
-&&
 #Unsafe compile temporarily needed due to a checker-framework issue
-./gradlew :vajram:vajram-codegen:publishToMavenLocal -PunsafeCompile=true --no-build-cache \
-&&
-sed -i '' 's/com.flipkart.krystal.previousVersion/com.flipkart.krystal.currentVersion/g' build.gradle \
-&&
-#Unsafe compile temporarily needed due to a checker-framework issue
+./gradlew :vajram:vajram-codegen:publishToMavenLocal -PunsafeCompile=true --no-build-cache &&\
 ./gradlew publishToMavenLocal -PunsafeCompile=true --no-build-cache
