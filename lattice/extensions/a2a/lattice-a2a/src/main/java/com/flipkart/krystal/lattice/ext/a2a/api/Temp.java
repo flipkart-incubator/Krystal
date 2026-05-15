82cead3824725da51f42ebb2119d49e3e6477b1e
Ram Anvesh <2082745+RamAnvesh@users.noreply.github.com>
2026-05-15 16:17:45 +0530

    Create A2A extension modules

---
+++ lattice/extensions/a2a/lattice-a2a/src/main/java/com/flipkart/krystal/lattice/ext/a2a/api/Temp.java
@@ -0,0 +1,10 @@
+package com.flipkart.krystal.lattice.ext.a2a.api;
+
+import org.a2aproject.sdk.spec.AgentCard;
+
+public class Temp {
+
+  public Temp() {
+    AgentCard agentCard = new AgentCard();
+  }
+}
