6f5a1230ace5916665160d36ca8fcace356f5d27
Ram Anvesh <2082745+RamAnvesh@users.noreply.github.com>
2026-07-26 01:02:09 +0530

    Address review comments

--- lattice/lattice-core/src/main/java/com/flipkart/krystal/lattice/krystex/KrystexDopantSpec.java
+++ lattice/lattice-core/src/main/java/com/flipkart/krystal/lattice/krystex/KrystexDopantSpec.java
@@ -4,7 +4,6 @@ import static com.flipkart.krystal.lattice.krystex.KrystexDopant.DOPANT_TYPE;
 import static java.util.Objects.requireNonNullElse;
 
 import com.flipkart.krystal.krystex.KrystexGraph.KrystexGraphBuilder;
-import com.flipkart.krystal.krystex.kryon.DependentChain;
 import com.flipkart.krystal.krystex.kryon.KryonExecutorConfigurator;
 import com.flipkart.krystal.lattice.core.doping.SimpleDopantSpec;
 import com.flipkart.krystal.lattice.core.doping.SimpleDopantSpecBuilder;
@@ -13,9 +12,7 @@ import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.function.Consumer;
 import lombok.Builder;
 import lombok.Singular;
@@ -49,9 +46,6 @@ public record KrystexDopantSpec(
     @SuppressWarnings("MismatchedQueryAndUpdateOfCollection") // Used by lombok
     private final List<TraitDispatchPolicy> traitDispatchPolicies = new ArrayList<>();
 
-    @SuppressWarnings("MismatchedQueryAndUpdateOfCollection") // Used by lombok
-    private final Set<DependentChain> disabledDependentChains = new LinkedHashSet<>();
-
     public KrystexDopantSpecBuilder traitDispatchPolicies(
         Collection<? extends TraitDispatchPolicy> traitDispatchPolicies) {
       this.traitDispatchPolicies.addAll(traitDispatchPolicies);
@@ -61,18 +55,6 @@ public record KrystexDopantSpec(
     public KrystexDopantSpecBuilder traitDispatchPolicies(
         TraitDispatchPolicy... traitDispatchPolicies) {
       this.traitDispatchPolicies.addAll(Arrays.asList(traitDispatchPolicies));
-      return this;
-    }
-
-    public KrystexDopantSpecBuilder disabledDependentChains(
-        Collection<? extends DependentChain> disabledDependentChains) {
-      this.disabledDependentChains.addAll(disabledDependentChains);
-      return this;
-    }
-
-    public KrystexDopantSpecBuilder disabledDependentChains(
-        DependentChain... disabledDependentChains) {
-      this.disabledDependentChains.addAll(Arrays.asList(disabledDependentChains));
       return this;
     }
   }
